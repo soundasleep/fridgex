21d36d2a0fc374c289c5bfa3eb4c8c4490effa16
soundasleep <soundasleep@421072f7-a652-0410-97f3-01d036f82573>
2008-09-15 02:41:59 +0000

    removing the "home page" and just making the product listing the home page

--- apps/fridge/modules/home/templates/indexSuccess.php
+++
@@ -1,4 +0,0 @@
-<h1>Welcome to the <?php echo sfConfig::get("app_title", "fridge"); ?></h1>
-
-<p><?php echo link_to("See available products", "product/list"); ?> or <?php echo link_to("login to your account", "security/index"); ?>.</p>
-
